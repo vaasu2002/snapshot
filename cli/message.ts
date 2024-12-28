b1d49cc1bb356e7cd7188a1c0caa383e5eda66cf
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2024-12-28 19:59:24 +0000

    util: added welcome and important messages for Snapshot utility

---
+++ cli/message.ts
@@ -0,0 +1,20 @@
+export const welcome= `
+**********************************************************
+*                                                        *
+*               WELCOME TO SNAPSHOT                      *
+*                                                        *
+**********************************************************
+*                                                        *
+*             A powerful database backup and             *
+*                  restore utility.                      *
+*                                                        *
+**********************************************************
+`;
+
+
+export  const important = `
+                                ⚠️ IMPORTANT ⚠️
+Please do not change the location of the snapshot folder.
+You should edit the configuration inside the \`config.json\` file located in the \`snapshot\` folder.
+Rest assured, your credentials are already added to .gitignore, ensuring they remain secure in production.
+`;
