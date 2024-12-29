15a37e98c4b4620d8510248b84972424ff41a2fd
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2024-12-29 15:14:05 +0000

    enhance cli initialization with interactive prompts for configuration

--- cli/index.ts
+++ cli/index.ts
@@ -2,8 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import yargs from 'yargs';
 
+import { yellow } from 'chalk';
+import { select } from '@inquirer/prompts';
 import {configFileOption} from './common';
 import { important, welcome } from './message';
+import {databaseChoices} from './common';
 import { ensureDirectoryExists, ensureFileExists, appendToFileIfNotExist } from './flie-operations';
 
 
@@ -25,13 +28,32 @@ const argv = yargs
   .command('init', 'Initialize the package', async () => {
     
     console.log(welcome); 
+    
+    // prompts
+    const databaseOps = await select({
+      message: 'Select the database:',
+      choices: databaseChoices,
+      default: 'mysql',
+    });
 
+    const storageOps = await select({
+      message: 'Select the storage:',
+      choices: [
+        { name: 'Local', value: 'local' },
+        { name: 'Cloud', value: 'cloud' },
+      ],
+      default: 'local',
+    });
+
+    // edit to config as per option selected from prompts
+    configFileOption.database.dialect = databaseOps;
+    configFileOption.backup.storage = storageOps;
 
     // updating config
     fs.writeFileSync(configFilePath, JSON.stringify(configFileOption, null, 2));
     console.log(`config.json file created at ${configFilePath}`);
 
-    console.log(important);
+    console.log(yellow(important));
   })
   .argv;
 
